9f494e8f578d96a2064d255c89116bb3a1e2a1ac
fontilan <68748054+fontilan@users.noreply.github.com>
2022-07-28 20:06:08 +0200

    init basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,126 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="app">
+      <div className="hero">
+        <h1>
+          <span className="hero-line1">Wojciech</span>
+          <br />
+          <span className="hero-line2">Gmuzdek</span>
+        </h1>
+        <h2>
+          <span className="hero-line3">Frontend</span>
+          <br />
+          <span className="hero-line4">Developer</span>
+        </h2>
+      </div>
+      <div className="technologies">
+        <h3>
+          <span className="section-number">1.</span>
+          <span className="section-name"> Technologies</span>
+        </h3>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sed mi
+          dolor. Praesent suscipit eget lacus eget venenatis. Mauris in elit
+          ligula. Vestibulum dapibus at mauris varius blandit. Donec imperdiet
+          mattis orci id aliquet. Fusce ullamcorper fringilla tortor sit amet
+          molestie. Praesent eu posuere massa. Mauris ultricies neque felis, non
+          posuere dui hendrerit pharetra. Maecenas pellentesque justo arcu, at
+          laoreet odio pellentesque a.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>
+          Proin imperdiet quam erat, sit amet molestie mi sodales eu. Mauris
+          sodales ac libero sit amet pretium. Sed in urna sem. Nunc accumsan
+          mollis congue. Pellentesque pharetra malesuada tortor, ultricies
+          aliquam enim maximus gravida. Nunc bibendum finibus urna vitae
+          bibendum. Etiam consectetur neque sed suscipit vehicula. Maecenas id
+          purus a diam ornare ornare. Integer at dapibus tortor. Morbi et justo
+          imperdiet, molestie lectus ut, feugiat mi. Ut fermentum vehicula urna,
+          eget pellentesque justo porta a. Etiam laoreet massa nec mollis
+          gravida.
+        </p>
+        <p>
+          Praesent ultricies imperdiet sapien ac consectetur. Vivamus pharetra,
+          diam eu sollicitudin sollicitudin, mauris elit mollis ipsum, et
+          pellentesque metus purus id quam. Aliquam tristique sed mauris euismod
+          volutpat. Nam ac orci ac erat tempus posuere. Praesent sem massa,
+          dapibus eget tincidunt id, commodo eget velit. Vestibulum tristique mi
+          luctus elit egestas convallis. Vivamus luctus risus vel pulvinar
+          ultrices. Etiam fermentum nisi id malesuada tristique. Quisque quis
+          purus diam. Vivamus dictum sit amet massa ac laoreet.
+        </p>
+      </div>
+      <div className="projects">
+        <h3>
+          <span className="section-number">2.</span>
+          <span className="section-name"> Projects</span>
+        </h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sed mi
+          dolor. Praesent suscipit eget lacus eget venenatis. Mauris in elit
+          ligula. Vestibulum dapibus at mauris varius blandit. Donec imperdiet
+          mattis orci id aliquet. Fusce ullamcorper fringilla tortor sit amet
+          molestie. Praesent eu posuere massa. Mauris ultricies neque felis, non
+          posuere dui hendrerit pharetra. Maecenas pellentesque justo arcu, at
+          laoreet odio pellentesque a.
+        </p>
+        <p>
+          Proin imperdiet quam erat, sit amet molestie mi sodales eu. Mauris
+          sodales ac libero sit amet pretium. Sed in urna sem. Nunc accumsan
+          mollis congue. Pellentesque pharetra malesuada tortor, ultricies
+          aliquam enim maximus gravida. Nunc bibendum finibus urna vitae
+          bibendum. Etiam consectetur neque sed suscipit vehicula. Maecenas id
+          purus a diam ornare ornare. Integer at dapibus tortor. Morbi et justo
+          imperdiet, molestie lectus ut, feugiat mi. Ut fermentum vehicula urna,
+          eget pellentesque justo porta a. Etiam laoreet massa nec mollis
+          gravida.
+        </p>
+        <p>
+          Praesent ultricies imperdiet sapien ac consectetur. Vivamus pharetra,
+          diam eu sollicitudin sollicitudin, mauris elit mollis ipsum, et
+          pellentesque metus purus id quam. Aliquam tristique sed mauris euismod
+          volutpat. Nam ac orci ac erat tempus posuere. Praesent sem massa,
+          dapibus eget tincidunt id, commodo eget velit. Vestibulum tristique mi
+          luctus elit egestas convallis. Vivamus luctus risus vel pulvinar
+          ultrices. Etiam fermentum nisi id malesuada tristique. Quisque quis
+          purus diam. Vivamus dictum sit amet massa ac laoreet.
+        </p>
+      </div>
+      <div className="socials">
+        <h3>
+          <span className="section-number">3.</span>
+          <span className="section-name"> Socials</span>
+        </h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sed mi
+          dolor. Praesent suscipit eget lacus eget venenatis. Mauris in elit
+          ligula. Vestibulum dapibus at mauris varius blandit. Donec imperdiet
+          mattis orci id aliquet. Fusce ullamcorper fringilla tortor sit amet
+          molestie. Praesent eu posuere massa. Mauris ultricies neque felis, non
+          posuere dui hendrerit pharetra. Maecenas pellentesque justo arcu, at
+          laoreet odio pellentesque a.
+        </p>
+        <p>
+          Proin imperdiet quam erat, sit amet molestie mi sodales eu. Mauris
+          sodales ac libero sit amet pretium. Sed in urna sem. Nunc accumsan
+          mollis congue. Pellentesque pharetra malesuada tortor, ultricies
+          aliquam enim maximus gravida. Nunc bibendum finibus urna vitae
+          bibendum. Etiam consectetur neque sed suscipit vehicula. Maecenas id
+          purus a diam ornare ornare. Integer at dapibus tortor. Morbi et justo
+          imperdiet, molestie lectus ut, feugiat mi. Ut fermentum vehicula urna,
+          eget pellentesque justo porta a. Etiam laoreet massa nec mollis
+          gravida.
+        </p>
+        <p>
+          Praesent ultricies imperdiet sapien ac consectetur. Vivamus pharetra,
+          diam eu sollicitudin sollicitudin, mauris elit mollis ipsum, et
+          pellentesque metus purus id quam. Aliquam tristique sed mauris euismod
+          volutpat. Nam ac orci ac erat tempus posuere. Praesent sem massa,
+          dapibus eget tincidunt id, commodo eget velit. Vestibulum tristique mi
+          luctus elit egestas convallis. Vivamus luctus risus vel pulvinar
+          ultrices. Etiam fermentum nisi id malesuada tristique. Quisque quis
+          purus diam. Vivamus dictum sit amet massa ac laoreet.
+        </p>
+      </div>
     </div>
   );
 }
