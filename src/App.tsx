8e529b044ba2c38c86032799d3679cf9ba78b8bb
fontilan <68748054+fontilan@users.noreply.github.com>
2022-08-10 13:07:51 +0200

    move each section to a separate file

--- src/App.tsx
+++ src/App.tsx
@@ -1,162 +1,21 @@
+import About from './components/About';
+import Experience from './components/Experience';
+import Hero from './components/Hero';
+import Hobbies from './components/Hobbies';
+import Projects from './components/Projects';
+import Skills from './components/Skills';
+import Socials from './components/Socials';
+
 function App() {
   return (
     <div className="app">
-      <div className="hero">
-        <h1>
-          <span className="hero-line1">Wojciech</span>
-          <br />
-          <span className="hero-line2">Gmuzdek</span>
-        </h1>
-        <h2>
-          <span className="hero-line3">Frontend</span>
-          <br />
-          <span className="hero-line4">Developer</span>
-        </h2>
-      </div>
-      <div className="about">
-        <h3>
-          <span className="section-number">1.</span>
-          <span className="section-name"> About Me</span>
-        </h3>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sed mi
-          dolor. Praesent suscipit eget lacus eget venenatis. Mauris in elit
-          ligula. Vestibulum dapibus at mauris varius blandit. Donec imperdiet
-          mattis orci id aliquet. Fusce ullamcorper fringilla tortor sit amet
-          molestie. Praesent eu posuere massa. Mauris ultricies neque felis, non
-          posuere dui hendrerit pharetra. Maecenas pellentesque justo arcu, at
-          laoreet odio pellentesque a.
-        </p>
-        <p>
-          Proin imperdiet quam erat, sit amet molestie mi sodales eu. Mauris
-          sodales ac libero sit amet pretium. Sed in urna sem. Nunc accumsan
-          mollis congue. Pellentesque pharetra malesuada tortor, ultricies
-          aliquam enim maximus gravida. Nunc bibendum finibus urna vitae
-          bibendum. Etiam consectetur neque sed suscipit vehicula. Maecenas id
-          purus a diam ornare ornare. Integer at dapibus tortor. Morbi et justo
-          imperdiet, molestie lectus ut, feugiat mi. Ut fermentum vehicula urna,
-          eget pellentesque justo porta a. Etiam laoreet massa nec mollis
-          gravida.
-        </p>
-        <p>
-          Praesent ultricies imperdiet sapien ac consectetur. Vivamus pharetra,
-          diam eu sollicitudin sollicitudin, mauris elit mollis ipsum, et
-          pellentesque metus purus id quam. Aliquam tristique sed mauris euismod
-          volutpat. Nam ac orci ac erat tempus posuere. Praesent sem massa,
-          dapibus eget tincidunt id, commodo eget velit. Vestibulum tristique mi
-          luctus elit egestas convallis. Vivamus luctus risus vel pulvinar
-          ultrices. Etiam fermentum nisi id malesuada tristique. Quisque quis
-          purus diam. Vivamus dictum sit amet massa ac laoreet.
-        </p>
-      </div>
-      <div className="technologies">
-        <h3>
-          <span className="section-number">2.</span>
-          <span className="section-name"> Technologies</span>
-        </h3>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sed mi
-          dolor. Praesent suscipit eget lacus eget venenatis. Mauris in elit
-          ligula. Vestibulum dapibus at mauris varius blandit. Donec imperdiet
-          mattis orci id aliquet. Fusce ullamcorper fringilla tortor sit amet
-          molestie. Praesent eu posuere massa. Mauris ultricies neque felis, non
-          posuere dui hendrerit pharetra. Maecenas pellentesque justo arcu, at
-          laoreet odio pellentesque a.
-        </p>
-        <p>
-          Proin imperdiet quam erat, sit amet molestie mi sodales eu. Mauris
-          sodales ac libero sit amet pretium. Sed in urna sem. Nunc accumsan
-          mollis congue. Pellentesque pharetra malesuada tortor, ultricies
-          aliquam enim maximus gravida. Nunc bibendum finibus urna vitae
-          bibendum. Etiam consectetur neque sed suscipit vehicula. Maecenas id
-          purus a diam ornare ornare. Integer at dapibus tortor. Morbi et justo
-          imperdiet, molestie lectus ut, feugiat mi. Ut fermentum vehicula urna,
-          eget pellentesque justo porta a. Etiam laoreet massa nec mollis
-          gravida.
-        </p>
-        <p>
-          Praesent ultricies imperdiet sapien ac consectetur. Vivamus pharetra,
-          diam eu sollicitudin sollicitudin, mauris elit mollis ipsum, et
-          pellentesque metus purus id quam. Aliquam tristique sed mauris euismod
-          volutpat. Nam ac orci ac erat tempus posuere. Praesent sem massa,
-          dapibus eget tincidunt id, commodo eget velit. Vestibulum tristique mi
-          luctus elit egestas convallis. Vivamus luctus risus vel pulvinar
-          ultrices. Etiam fermentum nisi id malesuada tristique. Quisque quis
-          purus diam. Vivamus dictum sit amet massa ac laoreet.
-        </p>
-      </div>
-      <div className="projects">
-        <h3>
-          <span className="section-number">3.</span>
-          <span className="section-name"> Projects</span>
-        </h3>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sed mi
-          dolor. Praesent suscipit eget lacus eget venenatis. Mauris in elit
-          ligula. Vestibulum dapibus at mauris varius blandit. Donec imperdiet
-          mattis orci id aliquet. Fusce ullamcorper fringilla tortor sit amet
-          molestie. Praesent eu posuere massa. Mauris ultricies neque felis, non
-          posuere dui hendrerit pharetra. Maecenas pellentesque justo arcu, at
-          laoreet odio pellentesque a.
-        </p>
-        <p>
-          Proin imperdiet quam erat, sit amet molestie mi sodales eu. Mauris
-          sodales ac libero sit amet pretium. Sed in urna sem. Nunc accumsan
-          mollis congue. Pellentesque pharetra malesuada tortor, ultricies
-          aliquam enim maximus gravida. Nunc bibendum finibus urna vitae
-          bibendum. Etiam consectetur neque sed suscipit vehicula. Maecenas id
-          purus a diam ornare ornare. Integer at dapibus tortor. Morbi et justo
-          imperdiet, molestie lectus ut, feugiat mi. Ut fermentum vehicula urna,
-          eget pellentesque justo porta a. Etiam laoreet massa nec mollis
-          gravida.
-        </p>
-        <p>
-          Praesent ultricies imperdiet sapien ac consectetur. Vivamus pharetra,
-          diam eu sollicitudin sollicitudin, mauris elit mollis ipsum, et
-          pellentesque metus purus id quam. Aliquam tristique sed mauris euismod
-          volutpat. Nam ac orci ac erat tempus posuere. Praesent sem massa,
-          dapibus eget tincidunt id, commodo eget velit. Vestibulum tristique mi
-          luctus elit egestas convallis. Vivamus luctus risus vel pulvinar
-          ultrices. Etiam fermentum nisi id malesuada tristique. Quisque quis
-          purus diam. Vivamus dictum sit amet massa ac laoreet.
-        </p>
-      </div>
-      <div className="socials">
-        <h3>
-          <span className="section-number">4.</span>
-          <span className="section-name"> Socials</span>
-        </h3>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sed mi
-          dolor. Praesent suscipit eget lacus eget venenatis. Mauris in elit
-          ligula. Vestibulum dapibus at mauris varius blandit. Donec imperdiet
-          mattis orci id aliquet. Fusce ullamcorper fringilla tortor sit amet
-          molestie. Praesent eu posuere massa. Mauris ultricies neque felis, non
-          posuere dui hendrerit pharetra. Maecenas pellentesque justo arcu, at
-          laoreet odio pellentesque a.
-        </p>
-        <p>
-          Proin imperdiet quam erat, sit amet molestie mi sodales eu. Mauris
-          sodales ac libero sit amet pretium. Sed in urna sem. Nunc accumsan
-          mollis congue. Pellentesque pharetra malesuada tortor, ultricies
-          aliquam enim maximus gravida. Nunc bibendum finibus urna vitae
-          bibendum. Etiam consectetur neque sed suscipit vehicula. Maecenas id
-          purus a diam ornare ornare. Integer at dapibus tortor. Morbi et justo
-          imperdiet, molestie lectus ut, feugiat mi. Ut fermentum vehicula urna,
-          eget pellentesque justo porta a. Etiam laoreet massa nec mollis
-          gravida.
-        </p>
-        <p>
-          Praesent ultricies imperdiet sapien ac consectetur. Vivamus pharetra,
-          diam eu sollicitudin sollicitudin, mauris elit mollis ipsum, et
-          pellentesque metus purus id quam. Aliquam tristique sed mauris euismod
-          volutpat. Nam ac orci ac erat tempus posuere. Praesent sem massa,
-          dapibus eget tincidunt id, commodo eget velit. Vestibulum tristique mi
-          luctus elit egestas convallis. Vivamus luctus risus vel pulvinar
-          ultrices. Etiam fermentum nisi id malesuada tristique. Quisque quis
-          purus diam. Vivamus dictum sit amet massa ac laoreet.
-        </p>
-      </div>
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Experience />
+      <Hobbies />
+      <Socials />
       <div className="blob-l"></div>
       <div className="blob-m"></div>
       <div className="blob-s"></div>
---
+++ src/components/Hero.tsx
@@ -0,0 +1,18 @@
+const Hero = () => {
+  return (
+    <div className="hero">
+      <h1>
+        <span className="hero-line1">Wojciech</span>
+        <br />
+        <span className="hero-line2">Gmuzdek</span>
+      </h1>
+      <h2>
+        <span className="hero-line3">Frontend</span>
+        <br />
+        <span className="hero-line4">Developer</span>
+      </h2>
+    </div>
+  );
+};
+
+export default Hero;
