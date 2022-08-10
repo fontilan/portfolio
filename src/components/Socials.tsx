004942f164814f35076cfce66c1d6a7977073a56
fontilan <68748054+fontilan@users.noreply.github.com>
2022-08-10 21:07:04 +0200

    improve basic layout

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -1,39 +1,24 @@
 const Socials = () => {
   return (
-    <div className="socials">
-      <h3>
-        <span className="section-name">Socials</span>
-      </h3>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sed mi
-        dolor. Praesent suscipit eget lacus eget venenatis. Mauris in elit
-        ligula. Vestibulum dapibus at mauris varius blandit. Donec imperdiet
-        mattis orci id aliquet. Fusce ullamcorper fringilla tortor sit amet
-        molestie. Praesent eu posuere massa. Mauris ultricies neque felis, non
-        posuere dui hendrerit pharetra. Maecenas pellentesque justo arcu, at
-        laoreet odio pellentesque a.
-      </p>
-      <p>
-        Proin imperdiet quam erat, sit amet molestie mi sodales eu. Mauris
-        sodales ac libero sit amet pretium. Sed in urna sem. Nunc accumsan
-        mollis congue. Pellentesque pharetra malesuada tortor, ultricies aliquam
-        enim maximus gravida. Nunc bibendum finibus urna vitae bibendum. Etiam
-        consectetur neque sed suscipit vehicula. Maecenas id purus a diam ornare
-        ornare. Integer at dapibus tortor. Morbi et justo imperdiet, molestie
-        lectus ut, feugiat mi. Ut fermentum vehicula urna, eget pellentesque
-        justo porta a. Etiam laoreet massa nec mollis gravida.
-      </p>
-      <p>
-        Praesent ultricies imperdiet sapien ac consectetur. Vivamus pharetra,
-        diam eu sollicitudin sollicitudin, mauris elit mollis ipsum, et
-        pellentesque metus purus id quam. Aliquam tristique sed mauris euismod
-        volutpat. Nam ac orci ac erat tempus posuere. Praesent sem massa,
-        dapibus eget tincidunt id, commodo eget velit. Vestibulum tristique mi
-        luctus elit egestas convallis. Vivamus luctus risus vel pulvinar
-        ultrices. Etiam fermentum nisi id malesuada tristique. Quisque quis
-        purus diam. Vivamus dictum sit amet massa ac laoreet.
-      </p>
-    </div>
+    <>
+      <div className="card">
+        <div className="card--title">
+          <h3>Socials</h3>
+        </div>
+        <div className="card--items">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sed
+            mi dolor. Praesent suscipit eget lacus eget venenatis. Mauris in
+            elit ligula. Vestibulum dapibus at mauris varius blandit. Donec
+            imperdiet mattis orci id aliquet. Fusce ullamcorper fringilla tortor
+            sit amet molestie. Praesent eu posuere massa. Mauris ultricies neque
+            felis, non posuere dui hendrerit pharetra. Maecenas pellentesque
+            justo arcu, at laoreet odio pellentesque a.
+          </p>
+        </div>
+      </div>
+      <hr />
+    </>
   );
 };
 
