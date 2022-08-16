6d40942514148789611252601bc69460d44eae69
fontilan <68748054+fontilan@users.noreply.github.com>
2022-08-16 19:09:13 +0200

    add hobbies

--- src/components/Hobbies.tsx
+++ src/components/Hobbies.tsx
@@ -9,26 +9,168 @@ const Hobbies = () => {
           </h3>
         </div>
         <div className="card--items">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sed
-            mi dolor. Praesent suscipit eget lacus eget venenatis. Mauris in
-            elit ligula. Vestibulum dapibus at mauris varius blandit. Donec
-            imperdiet mattis orci id aliquet. Fusce ullamcorper fringilla tortor
-            sit amet molestie. Praesent eu posuere massa. Mauris ultricies neque
-            felis, non posuere dui hendrerit pharetra. Maecenas pellentesque
-            justo arcu, at laoreet odio pellentesque a.
-          </p>
-          <p>
-            Proin imperdiet quam erat, sit amet molestie mi sodales eu. Mauris
-            sodales ac libero sit amet pretium. Sed in urna sem. Nunc accumsan
-            mollis congue. Pellentesque pharetra malesuada tortor, ultricies
-            aliquam enim maximus gravida. Nunc bibendum finibus urna vitae
-            bibendum. Etiam consectetur neque sed suscipit vehicula. Maecenas id
-            purus a diam ornare ornare. Integer at dapibus tortor. Morbi et
-            justo imperdiet, molestie lectus ut, feugiat mi. Ut fermentum
-            vehicula urna, eget pellentesque justo porta a. Etiam laoreet massa
-            nec mollis gravida.
-          </p>
+          <div className="card--item">
+            <h4>Video Games</h4>
+            <p>
+              I mostly enjoy role-playing games, platformers, roguelikes and
+              roguelites, indie classics, unusual strategy games and any other
+              unorthodox and independent productions.
+            </p>
+            <div>
+              <p>My top 5 video games:</p>
+              <ul>
+                <li>Divinity: Original Sin 2</li>
+                <li>RimWorld</li>
+                <li>Witcher 3: Wild Hunt</li>
+                <li>Gwent: The Witcher Card Game</li>
+                <li>FTL: Faster Than Light</li>
+              </ul>
+            </div>
+          </div>
+          <hr />
+          <div className="card--item">
+            <h4>Films</h4>
+            <p>
+              I like horrors, science-fiction, thrillers, cult classics and
+              class B films. Mostly from the 70s and 80s.
+            </p>
+            <div>
+              <p>My top 5 horror films:</p>
+              <ul>
+                <li>
+                  <span className="work-title">The Cremator</span> (1969)
+                </li>
+                <li>
+                  <span className="work-title">Possession</span> (1981)
+                </li>
+                <li>
+                  <span className="work-title">The Thing</span> (1982)
+                </li>
+                <li>
+                  <span className="work-title">
+                    The Texas Chain Saw Massacre
+                  </span>{' '}
+                  (1974)
+                </li>
+                <li>
+                  <span className="work-title">Suspiria</span> (1977)
+                </li>
+              </ul>
+            </div>
+            <br />
+            <div>
+              <p>My top 5 sci-fi films:</p>
+              <ul>
+                <li>
+                  <span className="work-title">Videodrome</span> (1983)
+                </li>
+                <li>
+                  <span className="work-title">They Live</span> (1988)
+                </li>
+                <li>
+                  <span className="work-title">Children of Men</span> (2000)
+                </li>
+                <li>
+                  <span className="work-title">Alien</span> (1979)
+                </li>
+                <li>
+                  <span className="work-title">The Terminator</span> (1984)
+                </li>
+              </ul>
+            </div>
+            <hr />
+            <div className="card--item">
+              <h4>Literature</h4>
+              <p>
+                I enjoy modernist and postmodernist dramas and short stories. My
+                favourite authors include Franz Kafka, Herman Melville, William
+                Faulkner, H.P. Lovecraft, Stanisław Lem and Fyodor Dostoevsky. I
+                also read non-fiction from the realms of philosophy (Deleuze,
+                Nietzsche, Heidegger), psychoanalysis (Lacan, Freud, Jung), and
+                others (Kahneman, Harari, Sacks).
+              </p>
+              <div>
+                <p>My top 5 novels:</p>
+                <ul>
+                  <li>
+                    Salman Rushdie&nbsp;—&nbsp;
+                    <span className="work-title">Midnight’s Children</span>
+                  </li>
+                  <li>
+                    Miguel de Cervantes&nbsp;—&nbsp;
+                    <span className="work-title">Don Quixote</span>
+                  </li>
+                  <li>
+                    William Faulkner&nbsp;—&nbsp;
+                    <span className="work-title">Absalom, Absalom!</span>
+                  </li>
+                  <li>
+                    Fyodor Dostoevsky&nbsp;—&nbsp;
+                    <span className="work-title">The Idiot</span>
+                  </li>
+                  <li>
+                    Franz Kafka&nbsp;—&nbsp;
+                    <span className="work-title">The Trial</span>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <hr />
+            <div className="card--item">
+              <h4>Music</h4>
+              <p>
+                I love discovering new bands, new styles and new genres. Even
+                though there are band that I keep coming back to (The Beatles,
+                Radiohead, MF DOOM, Talking Heads, Beach House) I am constantly
+                on the search for something new. I enjoy making music playlists
+                to catalogue my discoveries (see the lusco-fusco app in the
+                Projects section or just click THIS LINK to jump there).
+              </p>
+              <div>
+                <p>My top 5 albums (at the moment):</p>
+                <ul>
+                  <li>
+                    Cosmic Analog Ensemble&nbsp;—&nbsp;
+                    <span className="work-title">Expo Botanica</span> (2022)
+                  </li>
+                  <li>
+                    Freddie Gibbs&nbsp;—&nbsp;
+                    <span className="work-title">Piñata</span> (2014)
+                  </li>
+                  <li>
+                    Dean Blunt&nbsp;—&nbsp;
+                    <span className="work-title">Black Metal 2</span> (2021)
+                  </li>
+                  <li>
+                    Telemakus&nbsp;—&nbsp;
+                    <span className="work-title">The New Heritage</span> (2021)
+                  </li>
+                  <li>
+                    Gwenno&nbsp;—&nbsp;
+                    <span className="work-title">Tresor</span> (2022)
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <hr />
+            <div className="card--item">
+              <h4>Other</h4>
+              <p>
+                Apart from the above I also enjoy working out (calisthenics),
+                playing with my cat Dżuma and of course spending time with my
+                friends, playing board games, watching films together or just
+                cooking, drinking and talking about nonsense.
+              </p>
+              <p>
+                When I feel inspired I like to write short stories about
+                completely fiction people in completely made-up scenarios.
+              </p>
+              <p>
+                There is a Rubik's cube on my desk, sometimes I solve it for
+                fun.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
       <hr />
