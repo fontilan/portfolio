c3e2f4c22ab0f32289fcf5c101b7802564b13cdd
fontilan <68748054+fontilan@users.noreply.github.com>
2022-08-31 16:27:18 +0200

    feat: update the hobbies section

--- src/components/Hobbies.tsx
+++ src/components/Hobbies.tsx
@@ -14,70 +14,24 @@ const Hobbies = () => {
             <p>
               I mostly enjoy role-playing games, platformers, roguelikes and
               roguelites, indie classics, card games and any other unorthodox
-              and independent productions.
+              productions. I am currently playing Pillars of Eternity II:
+              Deadfire as well as Gwent: The Witcher Card Game. According to
+              Steam, my other top played games are Path of Exile, RimWorld and
+              Warframe.
             </p>
-            <div>
-              <p>My top 5 video games:</p>
-              <ul>
-                <li>Divinity: Original Sin 2</li>
-                <li>RimWorld</li>
-                <li>Witcher 3: Wild Hunt</li>
-                <li>Gwent: The Witcher Card Game</li>
-                <li>FTL: Faster Than Light</li>
-              </ul>
-            </div>
           </div>
           <hr />
           <div className="card--item">
             <h4>Films</h4>
             <p>
-              I like horrors, science-fiction, thrillers, cult classics and
-              class B films. Mostly from the 70s and 80s.
+              My favourites are genre films - horrors, science-fiction,
+              thrillers, and action films. I also like cult classics and class B
+              films (aka "midnight flicks") and class Z films (aka "so bad it's
+              good"). I love the 70s and 80s aesthetics and themes, and also the
+              classic 30s German Expressionist vibes. My favourite films of the
+              year 2022 are Everything Everywhere All at Once, The Northman and
+              Watcher.
             </p>
-            <div>
-              <p>My top 5 horror films:</p>
-              <ul>
-                <li>
-                  <span className="work-title">The Cremator</span> (1969)
-                </li>
-                <li>
-                  <span className="work-title">Possession</span> (1981)
-                </li>
-                <li>
-                  <span className="work-title">The Thing</span> (1982)
-                </li>
-                <li>
-                  <span className="work-title">
-                    The Texas Chain Saw Massacre
-                  </span>{' '}
-                  (1974)
-                </li>
-                <li>
-                  <span className="work-title">Suspiria</span> (1977)
-                </li>
-              </ul>
-            </div>
-            <br />
-            <div>
-              <p>My top 5 sci-fi films:</p>
-              <ul>
-                <li>
-                  <span className="work-title">Videodrome</span> (1983)
-                </li>
-                <li>
-                  <span className="work-title">They Live</span> (1988)
-                </li>
-                <li>
-                  <span className="work-title">Children of Men</span> (2000)
-                </li>
-                <li>
-                  <span className="work-title">Alien</span> (1979)
-                </li>
-                <li>
-                  <span className="work-title">The Terminator</span> (1984)
-                </li>
-              </ul>
-            </div>
             <hr />
             <div className="card--item">
               <h4>Literature</h4>
@@ -89,31 +43,6 @@ const Hobbies = () => {
                 Nietzsche, Heidegger), psychoanalysis (Lacan, Freud, Jung), and
                 others (Kahneman, Harari, Sacks).
               </p>
-              <div>
-                <p>My top 5 novels:</p>
-                <ul>
-                  <li>
-                    Salman Rushdie&nbsp;—&nbsp;
-                    <span className="work-title">Midnight’s Children</span>
-                  </li>
-                  <li>
-                    Miguel de Cervantes&nbsp;—&nbsp;
-                    <span className="work-title">Don Quixote</span>
-                  </li>
-                  <li>
-                    William Faulkner&nbsp;—&nbsp;
-                    <span className="work-title">Absalom, Absalom!</span>
-                  </li>
-                  <li>
-                    Fyodor Dostoevsky&nbsp;—&nbsp;
-                    <span className="work-title">The Idiot</span>
-                  </li>
-                  <li>
-                    Franz Kafka&nbsp;—&nbsp;
-                    <span className="work-title">The Trial</span>
-                  </li>
-                </ul>
-              </div>
             </div>
             <hr />
             <div className="card--item">
@@ -123,34 +52,9 @@ const Hobbies = () => {
                 though there are bands that I keep coming back to (The Beatles,
                 Radiohead, MF DOOM, Talking Heads, Beach House) I am constantly
                 on the search for something new. I enjoy making music playlists
-                to catalogue my discoveries (see the lusco-fusco app in the
-                Projects section or just click THIS LINK to jump there).
+                to catalogue my discoveries (see my lusco-fusco app in the
+                Projects section or just click here to jump to that section).
               </p>
-              <div>
-                <p>My top 5 albums (at the moment):</p>
-                <ul>
-                  <li>
-                    Cosmic Analog Ensemble&nbsp;—&nbsp;
-                    <span className="work-title">Expo Botanica</span> (2022)
-                  </li>
-                  <li>
-                    Freddie Gibbs&nbsp;—&nbsp;
-                    <span className="work-title">Piñata</span> (2014)
-                  </li>
-                  <li>
-                    Dean Blunt&nbsp;—&nbsp;
-                    <span className="work-title">Black Metal 2</span> (2021)
-                  </li>
-                  <li>
-                    Telemakus&nbsp;—&nbsp;
-                    <span className="work-title">The New Heritage</span> (2021)
-                  </li>
-                  <li>
-                    Gwenno&nbsp;—&nbsp;
-                    <span className="work-title">Tresor</span> (2022)
-                  </li>
-                </ul>
-              </div>
             </div>
             <hr />
             <div className="card--item">
@@ -159,15 +63,9 @@ const Hobbies = () => {
                 Apart from the above I also enjoy working out (calisthenics),
                 playing with my cat Dżuma and of course spending time with my
                 friends - playing board games, watching films together or just
-                cooking, drinking and talking about nonsense.
-              </p>
-              <p>
-                When I feel inspired I like to write short stories about
-                completely fictional people in completely made-up scenarios.
-              </p>
-              <p>
-                There is a Rubik's cube on my desk, sometimes I solve it for
-                fun.
+                cooking, drinking and talking about nonsense. When I feel
+                inspired I like to write short stories about completely
+                fictional people in completely made-up scenarios.
               </p>
             </div>
           </div>
