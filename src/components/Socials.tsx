cb92bc8f9d089c6e0d4188224bf6a1b72b1bbf2f
fontilan <68748054+fontilan@users.noreply.github.com>
2022-08-31 17:59:45 +0200

    feat: add social media info

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -1,4 +1,23 @@
 const Socials = () => {
+  const ListItem = ({
+    activity,
+    websiteName,
+    link,
+  }: {
+    activity: string;
+    websiteName: string;
+    link: string;
+  }) => {
+    return (
+      <li>
+        {activity}? Check out my{' '}
+        <a href={link} target="_blank" rel="noreferrer">
+          {websiteName}
+        </a>
+      </li>
+    );
+  };
+
   return (
     <>
       <div className="card">
@@ -7,14 +26,36 @@ const Socials = () => {
         </div>
         <div className="card--items">
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sed
-            mi dolor. Praesent suscipit eget lacus eget venenatis. Mauris in
-            elit ligula. Vestibulum dapibus at mauris varius blandit. Donec
-            imperdiet mattis orci id aliquet. Fusce ullamcorper fringilla tortor
-            sit amet molestie. Praesent eu posuere massa. Mauris ultricies neque
-            felis, non posuere dui hendrerit pharetra. Maecenas pellentesque
-            justo arcu, at laoreet odio pellentesque a.
+            I'm not really a fan of the likes of Facebook, Twitter, Instagram
+            etc. I was once on Google+, guess how that worked out 😎
           </p>
+          <p>
+            If you want to get to know me a bit, you can check out my profiles
+            on the following websites (oh, and don't be a stranger - feel free
+            to send me a friend invite once you're there).
+          </p>
+          <ul>
+            <ListItem
+              activity="Films I watch"
+              websiteName="Letterboxd"
+              link="https://letterboxd.com/fontilan/"
+            />
+            <ListItem
+              activity="Music I listen to"
+              websiteName="Last.fm"
+              link="https://last.fm/user/sernicze/"
+            />
+            <ListItem
+              activity="Books I read"
+              websiteName="Goodreads"
+              link="https://www.goodreads.com/user/show/27280059-sernicze"
+            />
+            <ListItem
+              activity="Games I play"
+              websiteName="Steam"
+              link="https://steamcommunity.com/profiles/76561198062972504"
+            />
+          </ul>
         </div>
       </div>
       <hr />
