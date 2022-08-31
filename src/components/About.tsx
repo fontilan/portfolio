90772610d11006a8377cd68f272cde74e54390c8
fontilan <68748054+fontilan@users.noreply.github.com>
2022-08-31 16:00:55 +0200

    feat: add some info about me

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,24 +7,20 @@ const About = () => {
         </div>
         <div className="card--items">
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sed
-            mi dolor. Praesent suscipit eget lacus eget venenatis. Mauris in
-            elit ligula. Vestibulum dapibus at mauris varius blandit. Donec
-            imperdiet mattis orci id aliquet. Fusce ullamcorper fringilla tortor
-            sit amet molestie. Praesent eu posuere massa. Mauris ultricies neque
-            felis, non posuere dui hendrerit pharetra. Maecenas pellentesque
-            justo arcu, at laoreet odio pellentesque a.
+            Philosophy graduate of the Jagiellonian University with over 5 years
+            of professional experience in the field of customer service and
+            almost 2 years in administration/accounting. My priority in any work
+            is improving my qualifications, acquiring new knowledge and building
+            positive relations with others. I enjoy creative problem solving and
+            finding new ways to optimize my workflow.
           </p>
           <p>
-            Proin imperdiet quam erat, sit amet molestie mi sodales eu. Mauris
-            sodales ac libero sit amet pretium. Sed in urna sem. Nunc accumsan
-            mollis congue. Pellentesque pharetra malesuada tortor, ultricies
-            aliquam enim maximus gravida. Nunc bibendum finibus urna vitae
-            bibendum. Etiam consectetur neque sed suscipit vehicula. Maecenas id
-            purus a diam ornare ornare. Integer at dapibus tortor. Morbi et
-            justo imperdiet, molestie lectus ut, feugiat mi. Ut fermentum
-            vehicula urna, eget pellentesque justo porta a. Etiam laoreet massa
-            nec mollis gravida.
+            I am ready to start my new career as a frontend developer. I would
+            like to join a passionate creative team ready to share their
+            knowledge with me as I would love to learn all the best industry
+            practices. Through my work as a frontend developer I wish to make
+            the internet at least a little bit better, prettier and more
+            accessible.
           </p>
         </div>
       </div>
