d4c8585619b3bf36d8bfed744f7d3971b8ad1a11
fontilan <68748054+fontilan@users.noreply.github.com>
2022-09-09 12:51:35 +0200

    feat: add navbar

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,29 @@
 const Hero = () => {
+  const Navigation = () => {
+    return (
+      <ul className="navigation">
+        <li>
+          <a href="#Projects">PROJECTS</a>
+        </li>
+        <li>
+          <a href="#Experience">EXPERIENCE</a>
+        </li>
+        <li>
+          <a href="#Hobbies">HOBBIES</a>
+        </li>
+        <li>
+          <a href="#Contact">CONTACT</a>
+        </li>
+      </ul>
+    );
+  };
   return (
     <section className="hero">
-      <h1>Wojciech Gmuzdek</h1>
-      <h2>Frontend Developer</h2>
+      <div>
+        <h1>Wojciech Gmuzdek</h1>
+        <h2>Frontend Developer</h2>
+      </div>
+      <Navigation />
     </section>
   );
 };
