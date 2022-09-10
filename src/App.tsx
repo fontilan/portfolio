a401225eccf026bf8a35bdf3713ea1b25d2808a6
fontilan <68748054+fontilan@users.noreply.github.com>
2022-09-10 19:24:11 +0200

    remove hobbies and skills for now

--- src/App.tsx
+++ src/App.tsx
@@ -3,36 +3,10 @@ import Blobs from './blobs/Blobs';
 import Experience from './components/Experience';
 import Footer from './components/Footer';
 import Hero from './components/Hero';
-import Hobbies from './components/Hobbies';
+// import Hobbies from './components/Hobbies';
 import Projects from './components/Projects';
-import Skills from './components/Skills';
+// import Skills from './components/Skills';
 import Contact from './components/Contact';
-
-// values hardcoded for now, to be refactored
-// const Sidebar = () => {
-//   return (
-//     <ul className="sidebar">
-//       <li>
-//         <a href="#About">About Me</a>
-//       </li>
-//       <li>
-//         <a href="#Skills">Technologies & Skills</a>
-//       </li>
-//       <li>
-//         <a href="#Projects">Projects</a>
-//       </li>
-//       <li>
-//         <a href="#Experience">Experience</a>
-//       </li>
-//       <li>
-//         <a href="#Hobbies">Hobbies & Interests</a>
-//       </li>
-//       <li>
-//         <a href="#Socials">Online Presence</a>
-//       </li>
-//     </ul>
-//   );
-// };
 
 function App() {
   return (
@@ -40,10 +14,10 @@ function App() {
       <Blobs />
       <Hero />
       <About id="About" />
-      <Skills id="Skills" />
+      {/* <Skills id="Skills" /> */}
       <Projects id="Projects" />
       <Experience id="Experience" />
-      <Hobbies id="Hobbies" />
+      {/* <Hobbies id="Hobbies" /> */}
       <Contact id="Contact" />
       <Footer />
     </div>
