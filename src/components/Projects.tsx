7a8bbabac31fafc51b5f2c855cebc7ae2374d842
fontilan <68748054+fontilan@users.noreply.github.com>
2022-09-10 19:24:52 +0200

    feat: add first project

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,3 +1,5 @@
+const lusco = require('../images/lusco.webp');
+
 const Projects = ({ id }: { id: string }) => {
   return (
     <>
@@ -5,38 +7,35 @@ const Projects = ({ id }: { id: string }) => {
         <section className="card__title">
           <h3 id={id}>Projects</h3>
         </section>
-        <section className="card__items">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sed
-            mi dolor. Praesent suscipit eget lacus eget venenatis. Mauris in
-            elit ligula. Vestibulum dapibus at mauris varius blandit. Donec
-            imperdiet mattis orci id aliquet. Fusce ullamcorper fringilla tortor
-            sit amet molestie. Praesent eu posuere massa. Mauris ultricies neque
-            felis, non posuere dui hendrerit pharetra. Maecenas pellentesque
-            justo arcu, at laoreet odio pellentesque a.
-          </p>
-          <p>
-            Proin imperdiet quam erat, sit amet molestie mi sodales eu. Mauris
-            sodales ac libero sit amet pretium. Sed in urna sem. Nunc accumsan
-            mollis congue. Pellentesque pharetra malesuada tortor, ultricies
-            aliquam enim maximus gravida. Nunc bibendum finibus urna vitae
-            bibendum. Etiam consectetur neque sed suscipit vehicula. Maecenas id
-            purus a diam ornare ornare. Integer at dapibus tortor. Morbi et
-            justo imperdiet, molestie lectus ut, feugiat mi. Ut fermentum
-            vehicula urna, eget pellentesque justo porta a. Etiam laoreet massa
-            nec mollis gravida.
-          </p>
-          <p>
-            Praesent ultricies imperdiet sapien ac consectetur. Vivamus
-            pharetra, diam eu sollicitudin sollicitudin, mauris elit mollis
-            ipsum, et pellentesque metus purus id quam. Aliquam tristique sed
-            mauris euismod volutpat. Nam ac orci ac erat tempus posuere.
-            Praesent sem massa, dapibus eget tincidunt id, commodo eget velit.
-            Vestibulum tristique mi luctus elit egestas convallis. Vivamus
-            luctus risus vel pulvinar ultrices. Etiam fermentum nisi id
-            malesuada tristique. Quisque quis purus diam. Vivamus dictum sit
-            amet massa ac laoreet.
-          </p>
+        <section className="card__items project">
+          <h4 className="project__name">lusco-fusco</h4>
+          <div className="project__main">
+            <img
+              className="project__image"
+              src={lusco}
+              alt="screenshot of the lusco-fusco website"
+            />
+            <div className="project__links">
+              <a href="https://fontilan.github.io/lusco-fusco/">Website</a>
+              <a href="https://github.com/fontilan/lusco-fusco">GitHub</a>
+            </div>
+          </div>
+          <div className="project__description">
+            <p>
+              I love music, especially discovering new artists and styles. Some
+              time ago I've decided to challenge myself to create a fresh
+              playlist every month. It's a lot of fun and the playlist even
+              gained a few followers.
+            </p>
+            <p>
+              This is a simple showcase of the playlist, which I also update
+              every month (in a way it's an opportunity to work with colors). It
+              also has a cute light/dark theme switch (with an icon animation
+              that works absolutely not like it's supposed to, but I love it
+              nonetheless).
+            </p>
+            <p>Built with React, using SASS and ESLint.</p>
+          </div>
         </section>
       </article>
       <hr />
