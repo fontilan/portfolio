9b769ad23f4b76e4207a65ad04aa1b48e0ae5350
fontilan <68748054+fontilan@users.noreply.github.com>
2022-09-22 19:25:36 +0200

    test: use Netlify form

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,22 +1,22 @@
 const Contact = ({ id }: { id: string }) => {
-  const ListItem = ({
-    activity,
-    websiteName,
-    link,
-  }: {
-    activity: string;
-    websiteName: string;
-    link: string;
-  }) => {
-    return (
-      <li>
-        {activity}? Check out my{' '}
-        <a href={link} target="_blank" rel="noreferrer">
-          {websiteName}
-        </a>
-      </li>
-    );
-  };
+  // const ListItem = ({
+  //   activity,
+  //   websiteName,
+  //   link,
+  // }: {
+  //   activity: string;
+  //   websiteName: string;
+  //   link: string;
+  // }) => {
+  //   return (
+  //     <li>
+  //       {activity}? Check out my{' '}
+  //       <a href={link} target="_blank" rel="noreferrer">
+  //         {websiteName}
+  //       </a>
+  //     </li>
+  //   );
+  // };
 
   return (
     <>
@@ -25,13 +25,13 @@ const Contact = ({ id }: { id: string }) => {
           <h3 id={id}>Contact</h3>
         </section>
         <section className="card__items">
-          <p>
+          {/* <p>
             I&apos;m not really a fan of the likes of Facebook, Twitter,
             Instagram etc. I was once on Google+, guess how that worked out 😎
           </p>
           <p>
             If you want to get to know me a bit, you can check out my profiles
-            on the following websites (oh, and don&apos;t be a stranger — feel
+            on the following websites (oh, and don&apos;t be a stranger - feel
             free to send me a friend invite once you&apos;re there).
           </p>
           <ul>
@@ -55,7 +55,36 @@ const Contact = ({ id }: { id: string }) => {
               websiteName="Steam"
               link="https://steamcommunity.com/profiles/76561198062972504"
             />
-          </ul>
+          </ul> */}
+          <form name="contact" method="POST" data-netlify="true">
+            <p>
+              <label>
+                Your Name: <input type="text" name="name" />
+              </label>
+            </p>
+            <p>
+              <label>
+                Your Email: <input type="email" name="email" />
+              </label>
+            </p>
+            <p>
+              <label>
+                Your Role:{' '}
+                <select name="role[]" multiple>
+                  <option value="leader">Leader</option>
+                  <option value="follower">Follower</option>
+                </select>
+              </label>
+            </p>
+            <p>
+              <label>
+                Message: <textarea name="message"></textarea>
+              </label>
+            </p>
+            <p>
+              <button type="submit">Send</button>
+            </p>
+          </form>
         </section>
       </article>
       <hr />
