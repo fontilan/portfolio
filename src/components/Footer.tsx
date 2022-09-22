b59ef1912e09be9bc13c16b021d74f021a0f70ab
fontilan <68748054+fontilan@users.noreply.github.com>
2022-09-22 22:54:56 +0200

    content: add name and icon links to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,29 @@
+import githubIcon from '../icons/githubIcon';
+import linkedInIcon from '../icons/linkedInIcon';
+
 const Footer = () => {
-  return <footer>Last updated September 2022</footer>;
+  return (
+    <footer>
+      <p>&copy;Wojciech Gmuzdek</p>
+      <p>Last updated September 2022</p>
+      <div>
+        <a
+          className="icon-link"
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/fontilan">
+          {githubIcon}
+        </a>
+        <a
+          className="icon-link"
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.linkedin.com/in/wojciech-gmuzdek/">
+          {linkedInIcon}
+        </a>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
