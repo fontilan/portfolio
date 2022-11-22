bb5ef3187fe083ef748fe130e71397e64235e57f
fontilan <68748054+fontilan@users.noreply.github.com>
2022-11-22 21:54:42 +0100

    feat: function to display current year

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,11 +3,14 @@ import linkedInIcon from '../icons/linkedInIcon';
 
 const Footer = () => {
   const currentMonth = new Date().toLocaleString('default', { month: 'long' });
+  const currentYear = new Date().toLocaleString('default', { year: 'numeric' });
 
   return (
     <footer className="footer">
       <p>&copy;Wojciech Gmuzdek</p>
-      <p>{currentMonth} 2022</p>
+      <p>
+        {currentMonth} {currentYear}
+      </p>
       <div className="footer__icons">
         <a
           aria-label="GitHub profile page"
